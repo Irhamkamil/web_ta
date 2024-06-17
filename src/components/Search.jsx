c25ff575333557b03e0d92f7dc6fed4d70ec1b87
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2024-06-17 16:06:53 +0700

    Search and Tour Pages

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,14 +3,29 @@ import { Component } from "react";
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import ProductsModels from "../models/products";
-
+import { Navigate } from "react-router-dom";
 export default class Search extends Component {
   state = {
-    titles: []
+    titles: [],
+    search_params: null,
+    redirectAfterSubmit: false
   }
 
   componentDidMount() {
+    this.setDefaultValue()
     this.listProducts() 
+  }
+
+  setDefaultValue = () => {
+    // search params from url
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // get q from url
+    const query = urlParams.get("q");
+
+    if (['', null, undefined].includes(query) === false) {
+      this.setState({ search_params: query })
+    }
   }
 
   listProducts = async () => {
@@ -24,29 +39,37 @@ export default class Search extends Component {
     })
   };
 
+  searchHandle = () => {
+    this.setState({ redirectAfterSubmit: true })
+  }
+
   render() {
+    if (this.state.redirectAfterSubmit) {return <Navigate to={"/tour?q=" + this.state.search_params} />}
     return (
       <div className=" border rounded-2xl w-full h-48 bg-white1 shadow-2xl absolute">
         <div className="w-full h-full pl-4">
           <h2 className="px-14 py-10 h-24 font-bold text-2xl text-black">
             Find Your Package
           </h2>
-          <div className="px-14 flex justify-between gap-5">
-            {/* <input id="autoComplete" className="input max-w-full" placeholder="Find Destination"/> */}
-            <Autocomplete
-              id="free-solo-demo"
-              freeSolo
-              disableClearable
-              fullWidth
-              size="small"
-              options={this.state.titles}
-              renderInput={(params) => <TextField {...params} label="Find Your Destination" />}
-            />
+          <form className="px-14 flex justify-between gap-5" onSubmit={this.searchHandle}>
+              <Autocomplete
+                id="free-solo-demo"
+                freeSolo
+                disableClearable
+                fullWidth
+                size="small"
+                value={this.state.search_params}
+                onChange={(event, value) => {
+                  this.setState({ search_params: value })
+                }}
+                options={this.state.titles}
+                renderInput={(params) => <TextField {...params} label="Find Your Destination" />}
+              />
 
-            <button className="btn btn-md w-40 h-10 bg-secondary font-bold text-white1 relative ">
-              Search
-            </button>
-          </div>
+              <button className="btn btn-md w-40 h-10 bg-secondary font-bold text-white1 relative ">
+                Search
+              </button>
+            </form>
         </div>
       </div>
     );
--- src/pages/TourPage.jsx
+++ src/pages/TourPage.jsx
@@ -1,12 +1,81 @@
-import BestTourist from "../components/BestTourist";
+import { Component } from "react";
+import ProductsModels from "../models/products";
+import Search from "../components/Search";
+import Config from "../config/app";
+import Product from "../components/Product";
+import NotyServices from "../services/Noty";
+import { useEffect } from "react";
+export default class TourPage extends Component {
+  state = {
+    products: [],
+  };
 
-const TourPage = () => {
-  return (
-    <div className="mt-20 bg-white1">
-      Tour
-      <BestTourist />
-    </div>
-  );
+  listProducts = async () => {
+    // search params from url
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // get q from url
+    const query = urlParams.get("q");
+
+    // if query is not empty, get products by name
+    if (['', null, undefined].includes(query) === false) {
+      await ProductsModels.findBy("name", query).then((results) => {
+        const products = results.docs.map((doc) => doc.data());
+        this.setState({ products: products });
+      }).catch((error) => {
+        console.log(error)
+        NotyServices.error("Failed to get products data")
+      })
+
+    // if query is empty, get products directly
+    } else {
+      await ProductsModels.lists().then((results) => {
+        const products = results.docs.map((doc) => doc.data());
+        this.setState({ products: products });
+      }).catch((error) => {
+        console.log(error)
+        NotyServices.error("Failed to get products data")
+      })
+    }
+  };
+
+  componentDidMount() {
+    this.listProducts()
+  };
+
+  componentDidUpdate() {
+    this.listProducts()
+  };
+
+  render() {
+    return (
+      <div className="w-full h-[6500px] mt-20 bg-white1 relative">
+        <div className="w-full h-screen overflow-hidden bg-home bg-center lg:bg-cover bg-no-repeat brightness-75">
+          <div className="container mx-36 h-full w-8/12 gap-5 flex flex-col items-start justify-center brightness-200">
+            <h1 className="text-white text-4xl">
+              Domestic & International Tour Packages
+            </h1>
+            <p className="text-white text-xl text-justify w-7/12">
+              Now, holidays are becoming EASY, FAST, and FUN. {Config.app.name} provides
+              various options for tour packages, domestic and international tours
+              to various favorite destinations. Choose your package and experience
+              the convenience!
+            </p>
+          </div>
+        </div>
+        <div className="container w-10/12 h-48 mx-32 mt-[-100px] relative">
+          <Search />
+        </div>
+        <div className="container flex flex-col lg:w-full h-40 items-center justify-center gap-3 mt-16">
+          <h2 className="font-bold text-2xl text-black">The Best Tour Package</h2>
+          <p className="text-black font-medium">
+            The Best Tour Package Options Ready to Pamper Your Holiday
+          </p>
+        </div>
+        <div className="container w-full -mt-14">
+          <Product products={this.state.products} />
+        </div>
+      </div>
+    )
+  }
 };
-
-export default TourPage;
